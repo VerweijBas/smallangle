339701b0e8d5793d63eec0c6d9c5e105ae01bdfc
VerweijBas <bas.verweij@gmail.com>
2024-11-14 10:33:53 +0100

    i can now chooce wheter i want tan or sin with -help for each

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -5,30 +5,47 @@ import pandas as pd
 
 
 def sin(number):
+    """this function calculates the sin values between
+    0 and 2pi
+
+    Args:
+        number (int): the amount of steps the sin value is
+        calculated between 0 and 2pi
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
 def tan(number):
+    """this function calculates the tan values between
+    0 and 2pi
+
+    Args:
+        number (int): the amount of steps the tan value is
+        calculated between 0 and 2pi
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 
-@click.group()
+@click.group(help='this will give you values of sin or tan in amount of steps you choose within range 0 tot 2pi')
 def smallangle():
+    '''defines the function to be able to determine tan or sin based on choise'''
     pass
 
 @click.command()
 @click.option('-n', default=10, help='gives sin values between 0 and 2pi')
 def sin_values(n):
+    ''''generates sin values in certain amount of steps'''
     sin(n)
 
 @click.command()
 @click.option('-n', default=10, help='gives tan values between 0 and 2pi')
 def tan_values(n):
+    ''''generates tan values in certain amount of steps'''
     tan(n)
 
 
